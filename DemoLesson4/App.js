22a3a6ef0fc9d537568641ede8b1f982bad22088
nvdhai2003 <nvdhai2003@gmail.com>
2023-05-26 12:59:25 +0700

    first commit

---
+++ DemoLesson4/App.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from "./screen/Home";
+import Profile from "./screen/Profile";
+
+const Stack = createNativeStackNavigator();
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name={"Home"} component={Home}/>
+                <Stack.Screen name={"Profile"} component={Profile}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
